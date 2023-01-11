8f80ab9916b3451bedbd65dc60ea201a4da9ae49
Yaekob Demisse <94002610+Jamescog@users.noreply.github.com>
2023-01-11 11:38:33 +0300

    Create leetcode_P1877.py

---
+++ leetcode_P1877.py
@@ -0,0 +1,34 @@
+"""
+Question Statement
+The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+Each element of nums is in exactly one pair, and
+The maximum pair sum is minimized.
+Return the minimized maximum pair sum after optimally pairing up the elements.
+
+Time complexity
+===============================
+O(n log n)
+===============================
+Space complexity
+===============================
+O(n)
+===============================
+Approach
+===============================
+Brute Force
+"""
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        # Sort the nums
+        nums.sort()
+        # Pair the numbers that their sum would be minimum
+        pair = [(nums[i], nums[-i-1]) for i in range(len(nums) // 2)]
+        pair_sum = [sum(i) for i in pair]
+        
+        # Return the maximun sum
+        return max(pair_sum)
+
