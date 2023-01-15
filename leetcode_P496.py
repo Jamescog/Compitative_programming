1d193c10e93afcebe51d6f7035eb1f759615d99e
Yaekob Demisse <94002610+Jamescog@users.noreply.github.com>
2023-01-15 18:42:20 +0300

    Create leetcode_P496.py

---
+++ leetcode_P496.py
@@ -0,0 +1,41 @@
+"""
+Question Statement
+=================================================================
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+================================================================
+Time complexity
+================================================================
+O(n)
+=================================================================
+Space complexity
+===================================================================
+O(n)
+==================================================================
+Approach
+==================================================================
+Stack
+===================================================================
+"""
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # create a dictionary to store the next greater element for each element in nums2
+        m = {} 
+        stack = []
+        for num in nums2:
+            while stack and stack[-1] < num:
+                m[stack.pop()] = num
+            stack.append(num)
+        while stack:
+            m[stack.pop()] = -1
+        
+        ans = []
+        for num in nums1:
+            ans.append(m[num])
+        
+        return ans
